1f7bf8e46dc8c3727b12dc0fb56c415cf88d0abb
soheil <soheil_paper@yahoo.com>
2016-06-12 17:19:35 +0430

    Update main.c

--- main.c
+++ main.c
@@ -73,7 +73,9 @@
 
 #define PROGNAME "telegram-cli"
 #define VERSION "0.07"
-#define CONFIG_DIRECTORY  PROG_NAME
+#ifndef CONFIG_DIRECTORY
+    #define CONFIG_DIRECTORY "." PROG_NAME;
+#endif
 
 //#define CONFIG_FILE "/var/lib/openshift/571622c40c1e66cf8d000066/app-root/runtime/srv/telegram"
 /*
